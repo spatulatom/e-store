6c046e39d10a8691f410027be7418497f7d6438a
spatulatom <spatulatom@gmail.com>
2023-05-21 19:45:09 +0100

    read me

--- frontend/src/components/LoadingBox.js
+++ frontend/src/components/LoadingBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function LoadingBox() {
   return (
-    <div className="loading">
+    <div className="loading middle">
       <i className="fa fa-spinner fa-spin"></i> Loading...
     </div>
   );
